5f01976bfc5ecfe6874981cfa63b81e502c32d33
massimolunetta <massimo.lunetta@algomera.it>
2024-12-05 16:33:37 +0100

    edit echo

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -4,12 +4,17 @@ import Pusher from "pusher-js";
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-    // authEndpoint: "http://127.0.0.1:8000/api/broadcasting/auth",
     broadcaster: "pusher",
     key: import.meta.env.VITE_PUSHER_APP_KEY,
+    wsHost:
+        import.meta.env.VITE_PUSHER_HOST ??
+        `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
+    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
+    wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
+    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? "https") === "https",
+    enabledTransports: ["ws", "wss"],
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     forceTLS: true,
-    wsPort: 443,
 });
 
 console.log("Echo initialized");
